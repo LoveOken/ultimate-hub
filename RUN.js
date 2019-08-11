aa496b760a0b1336c92e7c0db58e2bba418f18dd
Lua Ran <cmeamet@protonmail.com>
2019-08-11 15:17:22 -0300

    saved port

--- RUN.js
+++ RUN.js
@@ -2,16 +2,6 @@ const { Socket } = require("./BACKEND/SOCKET-BACKEND.js");
 
 let SOCKET = new Socket(__dirname);
 
-<<<<<<< HEAD
-SOCKET.express.routeStart();
-SOCKET.socketStart();
-
-SOCKET.express.initialRoutingSetup();
-
-SOCKET.socketConnect();
-SOCKET.express.serverListen(2000);
-=======
 SOCKET.run(2000);
->>>>>>> heroku
 
 /* Use any port for local, Use process.env.PORT for Deploy in Heroku */
