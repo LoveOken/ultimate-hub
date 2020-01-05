6f0564d86860f9fb1cf437968a47ee7880255860
Lua Ran <cmeamet@protonmail.com>
2020-01-05 15:49:06 -0300

    complete rewrite untested

--- RUN.js
+++
@@ -1,7 +0,0 @@
-const { Socket } = require("./BACKEND/SOCKET-BACKEND.js");
-
-let SOCKET = new Socket(__dirname);
-
-SOCKET.run(2000);
-
-/* Use any port for local, Use process.env.PORT for Deploy in Heroku */
